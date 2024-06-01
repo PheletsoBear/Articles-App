d1b7b355f3dd1ef093e4c4d0be5ce1d5554e0b3c
mashalaneevanspheletso@gmail.com <mashalaneevanspheletso@gmail.com>
2024-06-01 07:45:01 +0200

    created services method for register

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RegisterRequestInterface } from '../types/registerRequest.interface';
+import { Observable, map } from 'rxjs';
+import { CurrentUserInterface } from '../../shared/types/currentUser.interface';
+import { authResponseInterface } from '../types/authRespnse.interface';
+import { response } from 'express';
+import { environment } from '../../../enviroments/enviroment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+   register(data: RegisterRequestInterface):Observable<CurrentUserInterface>{
+     const url = environment.apiUrl + '/users'
+     return this.http.post<authResponseInterface>(url,data).pipe(map((response) => response.user))
+   }
+
+}
