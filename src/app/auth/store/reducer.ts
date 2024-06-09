8c15bbe4c24c579e8130c6f3fed6e3af3cc057b8
mashalaneevanspheletso@gmail.com <mashalaneevanspheletso@gmail.com>
2024-06-09 13:04:00 +0200

    refactored and added comments

--- src/app/auth/store/reducer.ts
+++ src/app/auth/store/reducer.ts
@@ -2,19 +2,27 @@ import { createReducer, createFeature, on } from "@ngrx/store";
 import { authStateInterface } from "../types/authState.interface";
 import { authActions } from "./actions";
 
+//defines the initial state of the authentication module
 const initialState: authStateInterface = {
-  isSubmitting: false,
-  error: null,
+  isSubmitting: false, //intially this is false
+  error: null, //initially we do not have errors, only changes if there are errors
 };
 
+//create feature for the authentication module
 const authFeature = createFeature({
-  name: 'auth',
+  name: 'auth', //This is the authFeaure key 
   reducer: createReducer(
     initialState,
+    //handles the 'registration' action by setting isSubmitting to true and clearing errors
     on(authActions.register, (state) => ({ ...state, isSubmitting: true, error: null })),
+
+     //Handles the 'registerSuccess' action by resetting isSubmitting to false
     on(authActions.registerSuccess, (state) => ({ ...state, isSubmitting: false })),
+
+    //Handles the 'registerFailure' action by resetting isSubmitting to false and setting the error messae
     on(authActions.registerFailure, (state, action) => ({ ...state, isSubmitting: false, error: action.error }))
   ),
 });
 
+//Export the feature's key, reducer function, and seletors
 export const { name: authFeatureKey, reducer: authReducer, selectIsSubmitting } = authFeature;
