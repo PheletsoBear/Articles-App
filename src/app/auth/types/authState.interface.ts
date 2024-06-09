05fe1c169bb7b35653a5fe7f51b7acb2b632a9d0
mashalaneevanspheletso@gmail.com <mashalaneevanspheletso@gmail.com>
2024-06-09 12:43:07 +0200

    refactored and added coments

--- src/app/auth/types/authState.interface.ts
+++ src/app/auth/types/authState.interface.ts
@@ -1,4 +1,5 @@
+//this defines the structure of the state object
 export interface authStateInterface{
-    isSubmitting: boolean;
-    error : string | null
+    isSubmitting: boolean; //defines if the registration is in progress, helps in controlling the UI such as disabling button and spinners
+    error : string | null //this stores the error messages that might occur during the process of registration
 }
